ea055ee804e31ddecbf5f534f947d49626169fc8
Axel Enghamre <axel@enghamre.se>
2023-09-04 14:44:04 +0200

    added sign in functionality

---
+++ src/app/api/auth/sign-in/route.ts
@@ -0,0 +1,45 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { signInSchema } from "@/lib/schemas/AuthSchemas";
+
+import type { Database } from "@/lib/supabase";
+
+const dynamic = "force-dynamic";
+
+const POST = async (request: Request) => {
+  const supabase = createRouteHandlerClient<Database>({ cookies });
+  console.log("SUPABASE SIGN IN");
+
+  const data = await request.json();
+  console.log(data);
+
+  const validatedData = signInSchema.safeParse(data);
+
+  if (validatedData.success) {
+    const email = validatedData.data.email;
+    const password = validatedData.data.password;
+
+    const auth = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (auth.error) {
+      return new Response(JSON.stringify({ message: "Invalid user" }), {
+        status: 401,
+      });
+    }
+
+    if (auth.data) {
+      return new Response(JSON.stringify({ message: "Success logged in" }), {
+        status: 200,
+      });
+    }
+  }
+
+  return new Response(JSON.stringify({ message: "Invalid user data" }), {
+    status: 417,
+  });
+};
+
+export { POST, dynamic };
--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -1,16 +1,74 @@
 "use client";
 
 import { useState } from "react";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { signInSchema } from "@/lib/schemas/AuthSchemas";
 
 const SignIn = () => {
-  const supabase = createClientComponentClient();
-  const [signInFormValue, setSignInFormValue] = useState<{
-    email: string;
-    password: string;
-  }>();
+  const router = useRouter();
+  const [signInFormValue, setSignInFormValue] = useState({
+    email: "",
+    password: "",
+  });
+  const [isSigningIn, setIsSigningIn] = useState(false);
 
-  return <form action=""></form>;
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsSigningIn(true);
+
+    const validatedValues = signInSchema.safeParse(signInFormValue);
+
+    // CLIENT VALIDATION
+    if (validatedValues.success) {
+      try {
+        const res = await fetch("/api/auth/sign-in", {
+          method: "POST",
+          body: JSON.stringify(validatedValues.data),
+        });
+
+        if (res.ok) {
+          router.replace("/home");
+        }
+      } catch (error) {
+        // TODO show errors
+        console.log(error);
+      }
+    }
+
+    setIsSigningIn(false);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setSignInFormValue((signInFormValueSate) => ({
+      ...signInFormValueSate,
+      [name]: value,
+    }));
+
+    // TODO show errors
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        name="email"
+        value={signInFormValue.email}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={signInFormValue.password}
+        onChange={handleChange}
+      />
+      <button type="submit" disabled={isSigningIn}>
+        Logga in
+      </button>
+    </form>
+  );
 };
 
 export default SignIn;
