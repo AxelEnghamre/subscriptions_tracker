43f19812c9e1e85b72da97bf8963c2b0a01c72a7
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-09-06 11:02:54 +0200

    added profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+const metadata: Metadata = {
+  title: "Profil",
+};
+
+const ProfilePage = async () => {
+  <div>Din profil</div>;
+};
+
+export default ProfilePage;
+
+export { metadata };
