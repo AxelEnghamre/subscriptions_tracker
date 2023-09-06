2a7383635adae00f97376e736d8c94c6678ae0ad
Axel Enghamre <axel@enghamre.se>
2023-09-06 11:14:06 +0200

    added terms page

---
+++ src/app/help/terms/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+const metadata: Metadata = {
+  title: "Villkor",
+};
+
+const TermsPage = () => {
+  return <div>Villkor</div>;
+};
+
+export default TermsPage;
+
+export { metadata };
