f54d80e0e2500b831a425be97470173d283a3771
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-09-06 11:17:02 +0200

    created login information page

---
+++ src/app/help/login-information/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+const metadata: Metadata = {
+  title: "Inloggningsuppgifter",
+};
+
+const LoginInformationPage = async () => {
+  return <div>Dina Inloggningsuppgifter</div>;
+};
+
+export default LoginInformationPage;
+
+export { metadata };
