df61aff00b3680e50efadba60ae2d2486d6faf07
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-09-06 11:17:14 +0200

    created settings page

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+const metadata: Metadata = {
+  title: "Inställningar",
+};
+
+const SettingsPage = async () => {
+  return <div>Inställningar</div>;
+};
+
+export default SettingsPage;
+
+export { metadata };
