5ce65721adba1a82b17a4ad85fca5a4af8ae6f66
Axel Enghamre <axel@enghamre.se>
2023-09-06 11:11:45 +0200

    added cookies page

---
+++ src/app/help/cookies/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+const metadata: Metadata = {
+  title: "Cookies",
+};
+
+const CookiesPage = () => {
+  return <div>kakor</div>;
+};
+
+export default CookiesPage;
+
+export { metadata };
