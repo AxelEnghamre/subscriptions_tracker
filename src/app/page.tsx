8cbe69a7e539714a05343b3f634ec73755271f22
Axel Enghamre <axel@enghamre.se>
2023-09-08 10:26:21 +0200

    made user always be directed to sign-in on unAuth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,16 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import type { Database } from "@/lib/supabase";
 import { SignedInHome, SignOutHome } from "@/components/layouts/home";
+import { redirect } from "next/navigation";
 
 const Home = async () => {
   const supabase = createServerComponentClient<Database>({ cookies });
 
   const session = (await supabase.auth.getSession()).data.session;
+
+  if(!session) {
+    redirect("/sign-in");
+  }
 
   return (
     <main className="">{session ? <SignedInHome /> : <SignOutHome />}</main>
