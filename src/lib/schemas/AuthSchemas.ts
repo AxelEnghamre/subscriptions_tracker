179ec020023f3351ac0ea4765264dcb6475e3cdd
Axel Enghamre <axel@enghamre.se>
2023-09-11 11:20:20 +0200

    added swedish error messages and created sign up

--- src/lib/schemas/AuthSchemas.ts
+++ src/lib/schemas/AuthSchemas.ts
@@ -1,8 +1,20 @@
 import { z } from "zod";
 
 const signInSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(5),
+  email: z.string().email("Fel format"),
+  password: z.string().min(5, "För kort"),
 });
 
-export { signInSchema };
+const signUpSchema = z
+  .object({
+    name: z.string().min(2, "För kort").max(50, "För långt"),
+    email: z.string().email("Fel format"),
+    password: z.string().min(5, "För kort"),
+    confirmPassword: z.string().min(5, "För kort"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Lösenorden matchar inte",
+    path: ["confirmPassword"], // path of error
+  });
+
+export { signInSchema, signUpSchema };
