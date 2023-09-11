cb298e8fac606aab615a0c938d646b5206c68bc0
Axel Enghamre <axel@enghamre.se>
2023-09-11 10:52:38 +0200

    indented sign out route

--- src/app/api/auth/sign-out/route.ts
+++ src/app/api/auth/sign-out/route.ts
@@ -1,10 +1,10 @@
+const dynamic = "force-dynamic";
+
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 import type { Database } from "@/lib/supabase";
-
-const dynamic = "force-dynamic";
 
 const POST = async (request: Request) => {
   const requestUrl = new URL(request.url);
