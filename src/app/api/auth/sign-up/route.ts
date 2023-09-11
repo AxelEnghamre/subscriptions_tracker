1850b18ace53a3000b15438059a3339fbbc57780
Axel Enghamre <axel@enghamre.se>
2023-09-11 11:28:56 +0200

    created a sign up route

---
+++ src/app/api/auth/sign-up/route.ts
@@ -0,0 +1,40 @@
+const dynamic = "force-dynamic";
+
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+import { signUpSchema } from "@/lib/schemas/AuthSchemas";
+
+const POST = async (request: Request) => {
+  const supabase = createRouteHandlerClient({ cookies });
+  const data = await request.json();
+  const requestUrl = new URL(request.url);
+
+  const validatedData = signUpSchema.safeParse(data);
+
+  if (validatedData.success) {
+    const email = validatedData.data.email;
+    const name = validatedData.data.name;
+    const password = validatedData.data.password;
+
+    /* TODO: Implement name in the sign up */
+    await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${requestUrl.origin}/auth/callback`,
+      },
+    });
+
+    return NextResponse.redirect(requestUrl.origin, {
+      status: 301,
+    });
+  }
+
+  return new Response(JSON.stringify({ message: "Ogiltig data" }), {
+    status: 417,
+  });
+};
+
+export { POST, dynamic };
