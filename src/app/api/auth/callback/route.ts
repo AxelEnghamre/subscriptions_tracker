f5415a6705ffdf67f8d1b05b949dd1b7ff4b643e
Axel Enghamre <axel@enghamre.se>
2023-09-11 10:42:46 +0200

    indented callback route

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,11 +1,11 @@
+const dynamic = "force-dynamic";
+
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 import type { NextRequest } from "next/server";
 import type { Database } from "@/lib/supabase";
-
-const dynamic = "force-dynamic";
 
 const GET = async (request: NextRequest) => {
   const requestUrl = new URL(request.url);
