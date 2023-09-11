0b2db889d18f7f5d6d799fe1db6b46623b3cfc6f
Axel Enghamre <axel@enghamre.se>
2023-09-11 11:55:15 +0200

    added cookie for theme as well as state

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import Cookies from "js-cookie";
 
 type Themes = "light" | "dark";
 
 type ThemeContext = {
   theme: Themes;
-  changeThemeTo: (param: Themes)=> void;
+  changeThemeTo: (param: Themes) => void;
 };
 
 const ThemeContext = createContext<ThemeContext | null>(null);
@@ -14,16 +15,25 @@ const ThemeContext = createContext<ThemeContext | null>(null);
 const ThemeContextProvider = ({ children }: { children: React.ReactNode }) => {
   const [theme, setTheme] = useState<Themes>("light");
 
-  const changeThemeTo = (themeFromInput: Themes) => {
-    if(themeFromInput === "dark") {
+  useEffect(() => {
+    if (Cookies.get("theme") === "dark") {
+      setTheme("dark");
       document.body.classList.add("darkTheme");
+    }
+  }, []);
+
+  const changeThemeTo = (themeFromInput: Themes) => {
+    if (themeFromInput === "dark") {
+      document.body.classList.add("darkTheme");
+      Cookies.set("theme", "dark");
     } else {
       document.body.classList.remove("darkTheme");
+      Cookies.remove("theme");
     }
 
     // Rerender where the theme state
     setTheme(themeFromInput);
-  }
+  };
 
   return (
     <ThemeContext.Provider
