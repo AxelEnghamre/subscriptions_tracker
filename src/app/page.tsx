73fe71413be452013dea37e3805a4511e848d528
Axel Enghamre <axel@enghamre.se>
2023-09-14 13:05:07 +0200

    testing profile in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,12 +15,24 @@ const Home = async () => {
     redirect("/sign-in");
   }
 
-  return (
-    <main className="w-full h-full relative">
-      Home screen
-      <Menu />
-    </main>
-  );
+  const { data: services, error } = await supabase.schema("public").from("services").select("*").eq("is_public",true);
+
+  const {data: profile} = await supabase.schema("public").from("profiles").select("*").eq("id",session.user.id);
+
+        console.log(services);
+        console.log(profile);
+        console.log(error);
+
+        if(profile) {
+          return (
+            <main className="w-full h-full relative">
+              Home screen
+              {profile[0].name}
+              <Menu />
+            </main>
+          );
+        }
+        return;
 };
 
 export default Home;
