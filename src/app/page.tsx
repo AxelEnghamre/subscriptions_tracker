0d77bde1ad662c001824843a0bc8d0eeaf9a5440
Axel Enghamre <axel@enghamre.se>
2023-09-14 20:34:30 +0200

    Revert "added owner_id to services"
    This reverts commit 231484f25c4e277fd8f66540e5a717b244cf1370.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,29 +15,24 @@ const Home = async () => {
     redirect("/sign-in");
   }
 
-  const { data: services, error } = await supabase
-    .schema("public")
-    .from("services")
-    .select("*")
-    .eq("is_public", true);
+  const { data: services, error } = await supabase.schema("public").from("services").select("*").eq("is_public",true);
 
-  const { data: profile } = await supabase
-    .schema("public")
-    .from("profiles")
-    .select("*")
-    .eq("id", session.user.id)
-    .single();
+  const {data: profile} = await supabase.schema("public").from("profiles").select("*").eq("id",session.user.id);
 
-  console.log(services);
-  console.log(profile);
-  console.log(error);
+        console.log(services);
+        console.log(profile);
+        console.log(error);
 
-  return (
-    <main className="w-full h-full relative">
-      Home screen
-      <Menu />
-    </main>
-  );
+        if(profile) {
+          return (
+            <main className="w-full h-full relative">
+              Home screen
+              {profile[0].name}
+              <Menu />
+            </main>
+          );
+        }
+        return;
 };
 
 export default Home;
