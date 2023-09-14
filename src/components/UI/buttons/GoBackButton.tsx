f9a2906e95a91520cd3f6621c001b74beb28c6bd
Axel Enghamre <axel@enghamre.se>
2023-09-14 14:37:33 +0200

    fixed styling for theme button

--- src/components/UI/buttons/GoBackButton.tsx
+++ src/components/UI/buttons/GoBackButton.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import Image from "next/image";
+import { useContext } from "react";
+import { ThemeContext } from "@/contexts/ThemeContext";
 
 const GoBackButton = ({
   className,
@@ -9,13 +11,29 @@ const GoBackButton = ({
   className?: string;
   onClick: Function;
 }) => {
+  const { theme } = useContext(ThemeContext) as ThemeContext;
+
   const handleClick = () => {
     onClick();
   };
 
   return (
     <button onClick={handleClick} className={`relative w-10 h-10 ` + className}>
-      <Image src="/back.svg" alt="back ikon" fill />
+      <svg
+        width="40"
+        height="40"
+        viewBox="0 0 40 40"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M15.1111 10.2222L9 16.3333M9 16.3333L15.1111 22.4444M9 16.3333H18.7778C25.5281 16.3333 31 21.8052 31 28.5555V29.7777"
+          stroke={theme === "light" ? "#111344" : "#F6F6F6"}
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
     </button>
   );
 };
