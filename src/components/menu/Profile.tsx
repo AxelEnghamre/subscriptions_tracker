fea0162f33cdbed7e1e175bf2c3af4d76441f215
Axel Enghamre <axel@enghamre.se>
2023-09-14 22:13:03 +0200

    added profile name to profile segment in menu

--- src/components/menu/Profile.tsx
+++ src/components/menu/Profile.tsx
@@ -1,6 +1,14 @@
+"use client";
+
+import { useContext } from "react";
+import { UserContext } from "@/contexts/UserContext";
+
 const Profile = () => {
+  const {profile} = useContext(UserContext) as UserContext;
+
   return (
     <ul>
+      <li>{profile.name}</li>
       <li>Hantera hushåll</li>
       <li>Total premunationskostnad / månad</li>
       <li>Total premunationskostnad / år</li>
