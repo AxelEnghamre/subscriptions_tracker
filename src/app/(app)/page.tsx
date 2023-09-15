accbc9167b9839aad7b8546468a56d908b89d031
Axel Enghamre <axel@enghamre.se>
2023-09-15 13:17:26 +0200

    subsription link test

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,5 +1,10 @@
+import SubscriptionLink from "@/components/UI/links/SubscriptionLink";
+
 const Home = async () => {
-  return <>Home screen</>;
+  return (<div className="flex flex-col gap-5"><p>Home screen</p>
+  
+  <SubscriptionLink name={"netflix"} iconUrl="/lock.svg" pricePerMonth={300}/>
+  </div>);
 };
 
 export default Home;
