53fbf1f37bd36e2fc5fea18c071f2ff1c27fb710
Axel Enghamre <axel@enghamre.se>
2023-09-18 10:20:36 +0200

    styled subscription Link

--- src/components/UI/links/SubscriptionLink.tsx
+++ src/components/UI/links/SubscriptionLink.tsx
@@ -1,15 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
 
-const SubscriptionLink= ({name,iconUrl = "/eye.svg",pricePerMonth}:{name:string,iconUrl?:string,pricePerMonth:number}) => {
-    return (
-        <Link href={`/subscription/${name}`} className="w-24 h-24 p-4 rounded-2xl bg-white text-xs shadow-lg grid grid-rows-2 grid-cols-1">
-            <div className="relative h-full w-fit bg-black">
-                
-            </div>
-            <p className="w-full h-full ">{pricePerMonth} kr/mån</p>
-        </Link>
-    );
+const SubscriptionLink = ({
+  name,
+  iconUrl = "/unknown.svg",
+  pricePerMonth,
+}: {
+  name: string;
+  iconUrl?: string;
+  pricePerMonth: number;
+}) => {
+  return (
+    <Link
+      href={`/subscription/${name}`}
+      className="w-24 h-24 p-4 rounded-2xl bg-white text-xs shadow-lg flex flex-col items-center gap-1"
+    >
+      <div className="relative h-12 w-12">
+        <Image fill src={iconUrl} alt={name} />
+      </div>
+      <p className="text-center">{pricePerMonth} kr/mån</p>
+    </Link>
+  );
 };
 
 export default SubscriptionLink;
