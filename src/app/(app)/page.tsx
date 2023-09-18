ceab695dda95084ce208d7fda9b325076d1debf6
Axel Enghamre <axel@enghamre.se>
2023-09-18 10:41:48 +0200

    removed tests in app page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,21 +1,5 @@
-const dynamic = "force-dynamic";
-
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-import type { Database } from "@/lib/supabase";
-import SubscriptionLink from "@/components/UI/links/SubscriptionLink";
 
 const Home = async () => {
-  const supabase = createServerComponentClient<Database>({ cookies });
-
-  const session = (await supabase.auth.getSession()).data.session;
-
-  const { data: services } = await supabase
-    .schema("public")
-    .from("services")
-    .select("*")
-    .eq("is_public", true);
-
   return (
     <div className="flex flex-col gap-5">
       <p>Home screen</p>
@@ -26,5 +10,3 @@ const Home = async () => {
 };
 
 export default Home;
-
-export { dynamic };
