ee4ee87dc0a0d3f1a01a3c3152fdbdb547aa9c76
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-09-18 14:25:05 +0200

    updated category button

--- src/components/UI/buttons/CategoryButton.tsx
+++ src/components/UI/buttons/CategoryButton.tsx
@@ -1,5 +1,29 @@
-const CategoryButton = () => {
-  return <div>category</div>;
+import Image from "next/image";
+
+const CategoryButton = ({
+  className,
+  name,
+  source,
+  onClick,
+  id,
+}: {
+  className?: string;
+  name: string;
+  source: string;
+  onClick: Function;
+  id: string;
+}) => {
+  const handleClick = () => {
+    onClick();
+  };
+  return (
+    <li className={`${className} inline-block`} key={id} onClick={handleClick}>
+      <div className="flex flex-row px-4 py-2 gap-2">
+        <Image src={source} alt="icon" width={25} height={25} />
+        <p>{name}</p>
+      </div>
+    </li>
+  );
 };
 
 export default CategoryButton;
