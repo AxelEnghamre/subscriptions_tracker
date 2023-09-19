1c7864a5eb332f954e04b98db5b8cbf558ba342d
Axel Enghamre <axel@enghamre.se>
2023-09-19 09:18:24 +0200

    formatted app page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -6,46 +6,47 @@ import type { Database } from "@/lib/supabase";
 import SubscriptionLink from "@/components/UI/links/SubscriptionLink";
 
 const Home = async () => {
-
   const supabase = createServerComponentClient<Database>({ cookies });
 
   const session = (await supabase.auth.getSession()).data.session;
 
   if (!session) {
-    return (<p>No session</p>);
+    return <p>No session</p>;
   }
 
   const { data: subscriptions } = await supabase
-  .schema("public")
-  .from("users_subscriptions")
-  .select("*, subscriptions(*,services(*))")
-  .eq("user_id", session.user.id);
+    .schema("public")
+    .from("users_subscriptions")
+    .select("*, subscriptions(*,services(*),subscriptions_prices(*))")
+    .eq("user_id", session.user.id);
 
+console.log(subscriptions);
 
   return (
     <div className="flex flex-col gap-5">
       <p>Home screen</p>
- 
- {subscriptions? (
-  <ul>
-  {subscriptions.map((subscription)=>(
-    <li key={subscription.id}>
-      <SubscriptionLink 
-        name={subscription.subscriptions?.services?.name as string}
-        // iconUrl={subscription.subscriptions?.services?.icon_url as string}
-        pricePerMonth={subscription.subscriptions?.price_per_month as number}
-      />
-    </li>
-  ))}
- </ul>
- ) : (
-  <p>Du har inga abonemang</p>
- )}
-      
+
+      {subscriptions ? (
+        <ul>
+          {subscriptions.map((subscription) => (
+            <li key={subscription.id}>
+              <SubscriptionLink
+                name={subscription.subscriptions?.services?.name as string}
+                // iconUrl={subscription.subscriptions?.services?.icon_url as string}
+                pricePerMonth={
+                  subscription.subscriptions?.price_per_month as number
+                }
+              />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Du har inga abonemang</p>
+      )}
     </div>
   );
 };
 
 export default Home;
 
-export {dynamic};
+export { dynamic };
