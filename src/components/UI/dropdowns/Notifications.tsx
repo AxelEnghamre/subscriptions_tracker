22977280eb8b11949024f7938b7110bad39bb4da
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-09-21 09:27:55 +0200

    added notifications component

---
+++ src/components/UI/dropdowns/Notifications.tsx
@@ -0,0 +1,15 @@
+const Notifications = ({
+  className,
+  id,
+}: {
+  className?: string;
+  id: string;
+}) => {
+  return (
+    <div className={className} id={id}>
+      Notifications
+    </div>
+  );
+};
+
+export default Notifications;
