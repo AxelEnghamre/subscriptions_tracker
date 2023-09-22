4537b053a93589134e3c6483426f34a7b6008865
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-09-22 11:17:24 +0200

    created drop down container and added serach and notifications components

---
+++ src/components/UI/dropdowns/DropDownContainer.tsx
@@ -0,0 +1,13 @@
+import Notifications from "./Notifications";
+import Search from "./Serach";
+
+const DropDownContainer = () => {
+  return (
+    <div>
+      <Notifications className="hidden" />
+      <Search className="hidden" />
+    </div>
+  );
+};
+
+export default DropDownContainer;
