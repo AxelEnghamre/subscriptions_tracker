72dfd79bae78a6a81a925550846fb3a68f9afa5d
Axel Enghamre <axel@enghamre.se>
2023-09-23 11:31:08 +0100

    created subscription form

--- src/app/(app)/subscription/page.tsx
+++ src/app/(app)/subscription/page.tsx
@@ -1,10 +1,11 @@
 import GoHomeButton from "@/components/UI/buttons/GoHomeButton";
+import AddSubscriptionForm from "@/components/subscription/AddSubscriptionForm"
 
 const AddSubscription = () => {
     return (
         <>
             <GoHomeButton />
-            Add Subscription
+            <AddSubscriptionForm />
         </>
     );
 };
---
+++ src/components/subscription/AddSubscriptionForm.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Input from "@/components/UI/input/Input";
+import { useState } from "react";
+
+const AddSubscriptionForm = () => {
+    return (
+        <form>
+            {/* <Input 
+                name="name"
+                id="name"
+                type="text"
+                placeholder="Namn"
+                value=""
+                onChange={()=>{}}
+            /> */}
+
+            {/* name */}
+
+            {/*  */}
+        </form>
+    )
+};
+
+export default AddSubscriptionForm;
