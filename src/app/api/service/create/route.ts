90996d0c58b6dc9773fff2f65f4caec206f80ccd
Axel Enghamre <axel@enghamre.se>
2023-09-25 23:04:08 +0100

    Created APi endpoint for creating services
    But not no icon upload

---
+++ src/app/api/service/create/route.ts
@@ -0,0 +1,59 @@
+const dynamic = "force-dynamic";
+
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { serviceInputSchema } from "@/lib/schemas/ServiceSchema";
+
+import type { Database } from "@/lib/supabase";
+
+const POST = async (request: Request) => {
+  const supabase = createRouteHandlerClient<Database>({ cookies });
+  const data = await request.json();
+
+  const session = (await supabase.auth.getSession()).data.session;
+
+  if (!session) {
+    return new Response(
+      JSON.stringify({
+        message: "Du har inte behörighet till att skapa en tjänst",
+      }),
+      {
+        status: 417,
+      },
+    );
+  }
+
+  const serviceValidation = serviceInputSchema.safeParse(data);
+
+  if (!serviceValidation.success) {
+    return new Response(JSON.stringify({ message: "Ogiltig data" }), {
+      status: 417,
+    });
+  }
+
+  const serviceData = serviceValidation.data;
+
+  const { error: ServiceInsertError } = await supabase
+    .from("services")
+    .insert([
+      {
+        name: serviceData.name,
+        website_url: serviceData.websiteUrl,
+        category: serviceData.category,
+        owner_id: session.user.id,
+      },
+    ])
+    .select();
+
+  if (ServiceInsertError) {
+    return new Response(JSON.stringify({ message: "Server fel" }), {
+      status: 500,
+    });
+  }
+
+  return new Response(JSON.stringify({ message: "Tjänst skapad" }), {
+    status: 200,
+  });
+};
+
+export { POST, dynamic };
