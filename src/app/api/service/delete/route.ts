efc58951a5732a0bb820aff45ada1196c04ad867
Axel Enghamre <axel@enghamre.se>
2023-09-25 23:26:41 +0100

    created endpoit for deleting a service

---
+++ src/app/api/service/delete/route.ts
@@ -0,0 +1,53 @@
+const dynamic = "force-dynamic";
+
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { serviceInputDeleteSchema } from "@/lib/schemas/ServiceSchema";
+
+import type { Database } from "@/lib/supabase";
+
+const POST = async (request: Request) => {
+  const supabase = createRouteHandlerClient<Database>({ cookies });
+  const data = await request.json();
+
+  const session = (await supabase.auth.getSession()).data.session;
+
+  if (!session) {
+    return new Response(
+      JSON.stringify({
+        message: "Du har inte behörighet till att skapa en tjänst",
+      }),
+      {
+        status: 417,
+      },
+    );
+  }
+
+  const serviceValidation = serviceInputDeleteSchema.safeParse(data);
+
+  if (!serviceValidation.success) {
+    return new Response(JSON.stringify({ message: "Ogiltig data" }), {
+      status: 417,
+    });
+  }
+
+  const serviceData = serviceValidation.data;
+
+  const { error: ServiceDeleteError } = await supabase
+    .from("services")
+    .delete()
+    .eq("id", serviceData.id)
+    .select();
+
+  if (ServiceDeleteError) {
+    return new Response(JSON.stringify({ message: "Server fel" }), {
+      status: 500,
+    });
+  }
+
+  return new Response(JSON.stringify({ message: "Tjänst borttagen" }), {
+    status: 200,
+  });
+};
+
+export { POST, dynamic };
