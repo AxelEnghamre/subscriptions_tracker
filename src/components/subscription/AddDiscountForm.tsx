7bf426221b3470739c1fc8e259f662cf816faeba
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-09-28 13:52:47 +0200

    added discount form

--- src/components/subscription/AddDiscountForm.tsx
+++ src/components/subscription/AddDiscountForm.tsx
@@ -1,7 +1,24 @@
 "use client";
 
+import Input from "../UI/Input";
+
 const AddDisscountForm = () => {
-  return <div>Add Discount</div>;
+  return (
+    <form action="">
+      <label htmlFor="discountPrice">
+        <p>Rabbat pris</p>
+        {/* <Input
+          className="focus:outline-none bg-loginbar-foreground text-loginbar-surface font-inter"
+          id="discountPrice"
+          placeholder="59"
+          name="discountPrice"
+          type="number"
+          value={subscriptionFormValue.discountPrice}
+          onChange={handleChange}
+        /> */}
+      </label>
+    </form>
+  );
 };
 
 export default AddDisscountForm;
