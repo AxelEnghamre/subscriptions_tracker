6573342650106b0befeb0b027c5ee3316482aa05
Axel Enghamre <axel@enghamre.se>
2023-09-28 16:59:40 +0100

    added sendgrid as a cron job TODO

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,8 +1,34 @@
 const dynamic = "force-dynamic";
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
+
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(process.env.NEXT_SENDGRID_API_KEY as string);
 
 const GET = async () => {
-  return NextResponse.json({ ok: true });
-}
+  try {
+    const emails = ["axel@enghamre.se"];
+    const mail = {
+      to: emails,
+      from: process.env.NEXT_SENDGRID_SENDER_EMAIL as string,
+      subject: "priser",
+      text: `
+            ttest
+          `,
+    };
 
-export {GET,dynamic}
+    try {
+      await sgMail.send(mail);
+    } catch (error) {
+      // Error with sendgrid
+      return new Response(JSON.stringify(error), { status: 500 });
+    }
+  } catch (error) {
+    // Error with user data
+    return new Response(JSON.stringify(error), { status: 417 });
+  }
+
+  return NextResponse.json({ ok: true });
+};
+
+export { GET, dynamic };
