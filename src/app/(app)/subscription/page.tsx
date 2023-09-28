42a348ee82f3d55b37213eed0572d527853b2b3f
Axel Enghamre <axel@enghamre.se>
2023-09-28 08:17:32 +0100

    added data to add subscription

--- src/app/(app)/subscription/page.tsx
+++ src/app/(app)/subscription/page.tsx
@@ -1,7 +1,41 @@
 import GoHomeButton from "@/components/UI/buttons/GoHomeButton";
 import AddSubscriptionForm from "@/components/subscription/AddSubscriptionForm";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import type { Database } from "@/lib/supabase";
 
-const AddSubscription = () => {
+const AddSubscription = async () => {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const session = (await supabase.auth.getSession()).data.session;
+
+  if (!session) {
+    return <p>No session</p>;
+  }
+
+  const { data: subscriptions } = await supabase
+    .schema("public")
+    .from("subscriptions")
+    .select(
+      `
+      *
+      `,
+    )
+    .eq("owner_id", session.user.id);
+
+    const { data: services } = await supabase
+    .schema("public")
+    .from("services")
+    .select(
+      `
+      *
+      `,
+    )
+    .eq("owner_id", session.user.id);
+
+    console.log(subscriptions);
+    console.log(services)
+
   return (
     <>
       <div className="pb-2">
